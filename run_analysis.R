7f89d0b56794930bcbb07af189d5741fdefadb20
Otavio Nascimento <otanasc@gmail.com>
2015-09-22 22:35:37 +1000

    using dplyr, melt and dcast to tidy up and summarize the dataset

--- run_analysis.R
+++ run_analysis.R
@@ -7,13 +7,17 @@ labels <- fread("UCI HAR Dataset/activity_labels.txt", sep=" ", header=FALSE, co
 training.labels <- fread("UCI HAR Dataset/train/y_train.txt", header=FALSE, col.names=c("label"));
 training.subjects <- fread("UCI HAR Dataset/train/subject_train.txt", header=FALSE, col.names=c("subject"));
 training.dataset <- fread("UCI HAR Dataset/train/X_train.txt", sep=" ", header=FALSE, col.names=features[[2]]);
-training.dataset <- mutate(training.dataset,
-  subject = select(training.subjects, subject),
-  label = labels$label.name[match(training.labels$label, labels$label.num)]);
-
 test.labels <- fread("UCI HAR Dataset/test/y_test.txt", header=FALSE, col.names=c("label"));
 test.subjects <- fread("UCI HAR Dataset/test/subject_test.txt", header=FALSE, col.names=c("subject"));
 test.dataset <- fread("UCI HAR Dataset/test/X_test.txt", sep=" ", header=FALSE, col.names=features[[2]]);
-test.dataset <- mutate(test.dataset,
-  subject = select(test.subjects, subject),
-  label = labels$label.name[match(test.labels$label, labels$label.num)]);
+
+training.dataset <- mutate(select(training.dataset, matches("mean\\(\\)|std\\(\\)")),
+                          subject = select(training.subjects, subject),
+                          label = labels$label.name[match(training.labels$label, labels$label.num)]);
+test.dataset <- mutate(select(test.dataset, matches("mean\\(\\)|std\\(\\)")),
+                      subject = select(test.subjects, subject),
+                      label = labels$label.name[match(test.labels$label, labels$label.num)]);
+
+merged.dataset <- rbind(training.dataset, test.dataset);
+merged.dataset.melt <- melt(merged.dataset, id=c("subject", "label"), measure.vars=setdiff(names(merged.dataset), c("subject", "label")));
+summarized.dataset <- dcast(merged.dataset.melt, subject + label ~ variable, mean);
